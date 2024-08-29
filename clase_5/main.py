2878f761762446b95076674c42c6eb90a9700ade
unknown <hernandezgalvezalejandro@gmail.com>
2024-08-29 17:21:57 -0600

    array de inglés

--- clase_5/main.py
+++ clase_5/main.py
@@ -1,10 +1,12 @@
 import statistics
 
 historia = [8, 7, 5, 8, 5, 4, 3, 5, 6, 7, 8, 10, 9, 4.7, 7, 8, 6, 9, 7, 6]
+english = [8, 9, 10, 7, 9, 10, 9, 8, 7, 8, 9, 9, 8, 9, 9, 10, 9, 9, 8, 7]
 
-media = statistics.mean(historia)
+media_historia = statistics.mean(historia)
+mediana_historia = statistics.median(historia)
 
-print(media)
+print(mediana_historia)
 
 """ 
 La varianza de una poblacion de N mediciones es es promedio de los cuadrados 
