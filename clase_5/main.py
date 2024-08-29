d66c1985a274a68e0d29f2d22bf5f1f3ba6f8715
unknown <hernandezgalvezalejandro@gmail.com>
2024-08-29 17:12:47 -0600

    varianza teoria

---
+++ clase_5/main.py
@@ -0,0 +1,13 @@
+import statistics
+
+historia = [8, 7, 5, 8, 5, 4, 3, 5, 6, 7, 8, 10, 9, 4.7, 7, 8, 6, 9, 7, 6]
+
+media = statistics.mean(historia)
+
+print(media)
+
+""" 
+La varianza de una poblacion de N mediciones es es promedio de los cuadrados 
+de las desviaciones de las mediciones alrededor de su media m. La varoanza
+poblacional se detona con σcuadrada.
+"""
