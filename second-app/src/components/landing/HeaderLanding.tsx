fb5c9d038a6f254dfecdaad83be52be1b131635f
unknown <hernandezgalvezalejandro@gmail.com>
2025-05-08 15:02:57 -0600

    ssr

---
+++ second-app/src/components/landing/HeaderLanding.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HeaderLanding() {
+  return (
+    <div className='bg-[#03363a]'>
+      <header>
+        <h2>Zendesk</h2>
+      </header>
+    </div>
+  )
+}
