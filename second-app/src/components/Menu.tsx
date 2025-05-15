6e67ecf95c747a5714376e645c4e0339877e3315
unknown <hernandezgalvezalejandro@gmail.com>
2025-05-14 19:29:55 -0600

    saved

--- second-app/src/components/Menu.tsx
+++ second-app/src/components/Menu.tsx
@@ -5,9 +5,9 @@ export const Menu = () => {
     return (
         <menu className='w-72 mx-auto text-center pt-10'>
             <ul>
-                <li><Link href="/ssr-landing" className='mb-3 p-3 block w-full bg-slate-200 rounded hover:bg-slate-400 hover:text-white '>SSR Server Side Rendering</Link></li>
-                <li><Link href="/csr-password" className='mb-3 p-3 block w-full bg-slate-200 rounded hover:bg-slate-400 hover:text-white '>CSR Client Side Rendering</Link></li>
-                <li><Link href="/dr-dynamic" className='mb-3 p-3 block w-full bg-slate-200 rounded hover:bg-slate-400 hover:text-white '>DR Dinamyc rendering</Link></li>
+                {/* <li><Link href="/ssr-landing" className='mb-3 p-3 block w-full bg-slate-200 rounded hover:bg-slate-400 hover:text-white '>SSR Server Side Rendering</Link></li> */}
+                <li><Link href="/csr-password" className='mb-3 p-3 block w-full bg-slate-200 rounded hover:bg-slate-400 hover:text-white '>Generador de Contraseñas</Link></li>
+                <li><Link href="/dr-dynamic" className='mb-3 p-3 block w-full bg-slate-200 rounded hover:bg-slate-400 hover:text-white '>Dragon ball API</Link></li>
             </ul>
         </menu>
     )
